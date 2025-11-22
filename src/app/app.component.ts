57d26f861d586cec7c6423b1b5f6ec2d197c3df5
Niv Kapade <niv.kapade02@gmail.com>
2025-11-22 09:13:38 +0530

    Add html & ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = '5BillionStars';
+  showForm: boolean = false;
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+  openForm(){
+    this.showForm = true;
+  }
 }
